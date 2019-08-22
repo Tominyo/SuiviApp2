ec5e0889f79bc5206447a52dae89a58cde612987
tommy <tommy.miredin@gmail.com>
2019-08-22 01:47:40 -0400

    Ajout de la BDD (SQLITEDATABASE) - Modification du nb de page

--- app/src/main/java/com/example/tommy/suiviapp2/Commit.java
+++ app/src/main/java/com/example/tommy/suiviapp2/Commit.java
@@ -4,17 +4,34 @@ import java.util.Date;
 
 public class Commit {
 
+    public static final String TABLE_NAME = "commits";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_MESSAGE = "message";
+    public static final String COLUMN_TIME = "time";
+    public static final String COLUMN_ID_DATE = "id_date";
+
     private String date;
     private int id;
+    private int id_date;
     private String message;
+
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_ID_DATE + " INTEGER,"
+                    + COLUMN_MESSAGE + " TEXT, "
+                    + COLUMN_TIME + " TEXT"
+                    + ")";
 
     public Commit(){
 
     }
 
-    public Commit(String message, int id, String date){
+    public Commit(int id, int id_date, String message, String date){
         this.message = message;
         this.id = id;
+        this.id_date = id_date;
         this.date = date;
     }
 
@@ -40,5 +57,13 @@ public class Commit {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public int getId_date() {
+        return id_date;
+    }
+
+    public void setId_date(int id_date) {
+        this.id_date = id_date;
     }
 }
